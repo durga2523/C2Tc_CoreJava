852014f5c96cac6277ae0315674649ac6c7fa980
DURGA <DURGA@172.16.219.120>
2024-04-03 11:50:20 +0530

    First Commit

---
+++ C37_CoreJava/src/com/tns/dayone/FirstProgram.java
@@ -0,0 +1,9 @@
+package com.tns.dayone;
+
+public class FirstProgram {
+	public static void main(String[] args) {
+		System.out.println("First Program");
+		System.out.println("Welcome All!!");
+	}
+
+}
