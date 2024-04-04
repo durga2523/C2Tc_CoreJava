25695503ddde1c0bd0d73d3bf7d60b8fc05d4ec5
Durga devi.K <141257480+durga2523@users.noreply.github.com>
2024-04-04 12:56:19 +0530

    Delete C37_CoreJava/src/com/tns/dayone/FirstProgram.java

--- C37_CoreJava/src/com/tns/dayone/FirstProgram.java
+++
@@ -1,9 +0,0 @@
-package com.tns.dayone;
-
-public class FirstProgram {
-	public static void main(String[] args) {
-		System.out.println("First Program");
-		System.out.println("Welcome All!!");
-	}
-
-}
